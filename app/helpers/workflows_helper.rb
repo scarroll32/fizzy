70a59eb3413d12134289646864c30e95e1f245ab
Andy Smith <andy@37signals.com>
2025-04-10 17:42:17 -0500

    WIP pins

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,7 +1,7 @@
 module WorkflowsHelper
   def button_to_set_stage(card, stage)
     button_to card_stagings_path(card, stage_id: stage),
-        method: :post, class: [ "btn full-width justify-start workflow-stage txt-uppercase", { "workflow-stage--current": stage == card.stage } ],
+        method: :post, class: [ "btn justify-start workflow-stage txt-uppercase workflow-stage", { "workflow-stage--current": stage == card.stage } ],
         form_class: "flex align-center gap-half",
         data: { turbo_frame: "_top" } do
       tag.span class: "overflow-ellipsis" do
